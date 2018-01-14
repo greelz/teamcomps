5dcf09c9ac919b42a57fcd343ca4cb8f02d42b33
greelz <greeleymp@gmail.com>
2018-01-14 16:43:58 -0600

    Grab the next best champion for a given subset of champions.

--- database/app/routes/league_routes.js
+++ database/app/routes/league_routes.js
@@ -25,7 +25,8 @@ function parseRequest(req) {
     c3 = Number(req.query['champ3']);
     c4 = Number(req.query['champ4']);
     c5 = Number(req.query['champ5']);
-    return [c1, c2, c3, c4, c5].filter(Boolean);
+    champ_arr = [c1, c2, c3, c4, c5].filter(Boolean);
+    return champ_arr;
 }
 
 function getPrimeProductFromRequest(req) {
@@ -41,31 +42,51 @@ function getPrimeProductFromChampArray(champArray) {
     return prime_prod;
 }
 
+function getPrimeFactorization(val) {
+    var primeFactors = [], primeVal, idx;
+    if (val) {
+        for (idx = 0; idx < primes_up_to_5000.length; ++idx) {
+            primeVal = primes_up_to_5000[idx];
+            if (val % primeVal === 0) {
+                primeFactors.push(primeVal);
+                val /= primeVal;
+                idx = -1;
+            }
+            if (val === 1) return primeFactors;
+        }
+    }
+}
+
 function generateDataForChampComp(db, prime_prod, callback) {
     // We want to generate data for everything that we care about
     // Overall win percentage, total number of games
     // Every team comp, w/ { total_games: <n>, wins: <n>, ... }
     // Some way to dive into that value, maybe a get request key or something... for later
 
-    db.collection("games").find( { 'comp_key' : { $mod : [prime_prod, 0 ] } }).toArray(function(err, all_games) {
-        if (!err) {
-            var wins = 0, games = 0, winPercent = 0;
-            var champDictionary = { '_id': prime_prod, 'total_games': all_games.length };
-            for (i = 0; i < all_games.length; ++i) {
-                var champPrimeProd = getPrimeProductFromChampArray(all_games[i]['team_comp']);
-                if (champDictionary[champPrimeProd] === undefined) {
-                    champDictionary[champPrimeProd] = { 'wins': 0, 'total_games': 0, "champArray": all_games[i]['team_comp'] };
-                }
-                champDictionary[champPrimeProd]['total_games'] += 1;
-                if (all_games[i]["win"]) { 
-                    champDictionary[champPrimeProd]['wins'] += 1;
-                    wins += 1;
-                }
+    var now = console.time("generateDataForChampComp"), game, wins = 0, games = 0, winPercent = 0,
+            cursor = db.collection("games").find( { 'comp_key' : { $mod : [prime_prod, 0 ] } }),
+            numGames = 0, champDictionary, champPrimeProd;
+    champDictionary = { '_id': prime_prod };
+    cursor.each( (err, game) => {
+        // Let's stop at 20,000 games since that's a ton of results and
+        // won't really affect the results too much (IMO)
+        if (game && numGames < 20000) {
+            numGames += 1;
+            champPrimeProd = game['comp_key'];
+            if (champDictionary[champPrimeProd] === undefined) {
+                champDictionary[champPrimeProd] = { 'wins': 0, 'total_games': 0, "champArray": game['team_comp'] };
             }
-            winPercent = wins / all_games.length;
-            
+            champDictionary[champPrimeProd]['total_games'] += 1;
+            if (game['win']) { 
+                champDictionary[champPrimeProd]['wins'] += 1;
+                wins += 1;
+            }
+        }
+        else { // cursor is exhausted, return the results here
+            winPercent = wins / numGames;
             champDictionary["win_percent"] = winPercent;
-            db.collection("champData").insert(champDictionary);
+            champDictionary["total_games"] = numGames;
+            console.timeEnd("generateDataForChampComp");
             callback(champDictionary);
         }
     });
@@ -84,59 +105,91 @@ module.exports = function(app, db) {
         // We may want to look into getting results of only 3 or 4 champions, where the 5th champion is basically irrelevant. This would give us more results anyways... 
         // I think we want to do the prime factorization on the server here, but it doesn't really matter to me.
         function loopThroughResults(dic, givenChamps) {
-            var result = { "total_games": dic["total_games"], "win_percent": dic["win_percent"], "given_champs": givenChamps }
-            Object.keys(dic).forEach(function(key) {
+            var result = { "total_games": dic["total_games"], "win_percent": dic["win_percent"], "given_champs": givenChamps };
+            var keys = Object.keys(dic), len = keys.length, key;
+            for (var i = 0; i < len; ++i) {
+                key = keys[i];
                 var games = dic[key]["total_games"];
                 if (games) {
                     var wins = dic[key]["wins"];
-                    var champArray = dic[key]["champArray"];
-                    if (games && wins) {
+                    if (wins) {
                         var winPercent = wins / games;
                     }
+                    var champArray = dic[key]["champArray"];
                     result[champArray.join()] = { "wins": wins, "total_games": games };
                 }
-            });
+            }
             return result;
         }
 
         var prime_prod = getPrimeProductFromRequest(req);
         var givenChamps = parseRequest(req);
-        db.collection("champData").find( { "_id": prime_prod }).toArray(function(err, findResult) {
-            if (err) console.log(err);
-            else {
-                if (findResult.length === 0) {
-                    generateDataForChampComp(db, prime_prod, (champDic) => {
-                        res.json(loopThroughResults(champDic, givenChamps));
-                    });
-                }
-                else {
-                    // Because it's an array, we need to return the "first" (and only) result
-                    res.json(loopThroughResults(findResult[0], givenChamps));
-                }
-            }
+        generateDataForChampComp(db, prime_prod, (champDic) => {
+            res.json(loopThroughResults(champDic, givenChamps));
         });
     });
 
-    // Get all games
+    // Get all games and return a winning percentage
     app.get("/winpercentage", (req, res) => {
         var prime_prod = getPrimeProductFromRequest(req);
-        db.collection("champData").find( { "_id": prime_prod }).toArray(function(err, findResult) {
-            if (err) console.log(err);
-            else {
-                if (findResult.length === 0) {
-                    generateDataForChampComp(db, prime_prod, (champDic) => {
-                        var winPercent = champDic["win_percent"];
-                        var totalGames = champDic["total_games"];
-                        res.json( { "win_percent": winPercent, "total_games": totalGames });
+        generateDataForChampComp(db, prime_prod, (champDic) => {
+            var winPercent = champDic["win_percent"];
+            var totalGames = champDic["total_games"];
+            res.json( { "win_percent": winPercent, "total_games": totalGames });
+        });
+    });
+
+
+    // Provide the next best champ given a certain team composition
+    app.get("/nextbestchamp", (req, res) => {
+        // Returns the next best champion from a champion dictionary
+        function nextBestChamp(givenChamps, prime_prod, champDic) {
+            var champs = {}, keys = Object.keys(champDic), len = keys.length, key, subKey,
+                newChamps, bestTotalGames, bestWinPercent = 0, bestWins, bestChamp, i, j, champion, 
+                bestComps;
+            for (i = 0; i < len; ++i) {
+                key = keys[i];
+                if (champDic[key]["champArray"]) {
+                    newChamps = champDic[key]["champArray"].filter( (val) => {
+                        return givenChamps.indexOf(val) === -1;
                     });
-                }
-                else {
-                    // Because it's an array, we need to return the "first" (and only) result
-                    var winPercent = findResult[0]["win_percent"];
-                    var totalGames = findResult[0]["total_games"];
-                    res.json( { "win_percent": winPercent, "total_games": totalGames });
+                    if (newChamps) {
+                        for (j = 0; j < newChamps.length; ++j) {
+                            champion = newChamps[j];
+                            if (!(champion in champs)) {
+                                champs[champion] = { "wins": 0, "total_games": 0, "comps": {} };
+                            }
+                            champs[champion]["comps"][champDic[key]["champArray"]] = 1;
+                            champs[champion]["wins"] += champDic[key]["wins"];
+                            champs[champion]["total_games"] += champDic[key]["total_games"];
+                        }
+                    }
                 }
             }
+
+            keys = Object.keys(champs); 
+            len = keys.length;
+            for (i = 0; i < len; ++i) {
+                key = keys[i];
+                total_games = champs[key]["total_games"];
+                if (total_games > 10) {
+                    wins = champs[key]["wins"];
+                    winPercent = wins / total_games;
+                    if (winPercent > bestWinPercent) {
+                        bestWins = wins;
+                        bestWinPercent = winPercent;
+                        bestChamp = key;
+                        bestTotalGames = total_games;
+                        bestComps = champs[key]["comps"];
+                    }
+                }
+            }
+            return { "win_percent" : bestWinPercent, "champId" : bestChamp, "total_games" : bestTotalGames, "wins" : bestWins, "comps": bestComps };
+        }
+        var champArray = parseRequest(req);
+        var prime_prod = getPrimeProductFromChampArray(champArray);
+        generateDataForChampComp(db, prime_prod, (champDic) => {
+            res.json(nextBestChamp(champArray, prime_prod, champDic));
         });
     });
 }
--- js/teamcomp.js
+++ js/teamcomp.js
@@ -1,6 +1,6 @@
 var champions = ["Wukong", "Jax", "Fiddlesticks", "Shaco", "Warwick", "Xayah", "Nidalee", "Zyra", "Kled", "Brand", "Rammus", "Illaoi", "Corki", "Braum", "Darius", "Tryndamere", "Miss Fortune", "Yorick", "Xerath", "Sivir", "Riven", "Orianna", "Gangplank", "Malphite", "Poppy", "Karthus", "Jayce", "Nunu", "Trundle", "Graves", "Zoe", "Gnar", "Lux", "Shyvana", "Renekton", "Fiora", "Jinx", "Kalista", "Fizz", "Kassadin", "Sona", "Irelia", "Viktor", "Rakan", "Kindred", "Cassiopeia", "Maokai", "Ornn", "Thresh", "Kayle", "Hecarim", "Kha'Zix", "Olaf", "Ziggs", "Syndra", "Dr. Mundo", "Karma", "Annie", "Akali", "Volibear", "Yasuo", "Kennen", "Rengar", "Ryze", "Shen", "Zac", "Talon", "Swain", "Bard", "Sion", "Vayne", "Nasus", "Kayn", "Twisted Fate", "Cho'Gath", "Udyr", "Lucian", "Ivern", "Leona", "Caitlyn", "Sejuani", "Nocturne", "Zilean", "Azir", "Rumble", "Morgana", "Taliyah", "Teemo", "Urgot", "Amumu", "Galio", "Heimerdinger", "Anivia", "Ashe", "Vel'Koz", "Singed", "Skarner", "Varus", "Twitch", "Garen", "Blitzcrank", "Master Yi", "Elise", "Alistar", "Katarina", "Ekko", "Mordekaiser", "Lulu", "Camille", "Aatrox", "Draven", "Tahm Kench", "Pantheon", "Xin Zhao", "Aurelion Sol", "Lee Sin", "Taric", "Malzahar", "Lissandra", "Diana", "Tristana", "Rek'Sai", "Vladimir", "Jarvan IV", "Nami", "Jhin", "Soraka", "Veigar", "Janna", "Nautilus", "Evelynn", "Gragas", "Zed", "Vi", "Kog'Maw", "Ahri", "Quinn", "LeBlanc", "Ezreal"]
 
-var champions_dic = {"aatrox": 266, "ahri": 103, "akali": 84, "alistar": 12, "amumu": 32, "anivia": 34, "annie": 1, "ashe": 22, "aurelionsol": 136, "azir": 268, "bard": 432, "blitzcrank": 53, "brand": 63, "braum": 201, "caitlyn": 51, "camille": 164, "cassiopeia": 69, "chogath": 31, "corki": 42, "darius": 122, "diana": 131, "draven": 119, "drmundo": 36, "ekko": 245, "elise": 60, "evelynn": 28, "ezreal": 81, "fiddlesticks": 9, "fiora": 114, "fizz": 105, "galio": 3, "gangplank": 41, "garen": 86, "gnar": 150, "gragas": 79, "graves": 104, "hecarim": 120, "heimerdinger": 74, "illaoi": 420, "irelia": 39, "ivern": 427, "janna": 40, "jarvaniv": 59, "jax": 24, "jayce": 126, "jhin": 202, "jinx": 222, "kalista": 429, "karma": 43, "karthus": 30, "kassadin": 38, "katarina": 55, "kayle": 10, "kayn": 141, "kennen": 85, "khazix": 121, "kindred": 203, "kled": 240, "kogmaw": 96, "leblanc": 7, "leesin": 64, "leona": 89, "lissandra": 127, "lucian": 236, "lulu": 117, "lux": 99, "malphite": 54, "malzahar": 90, "maokai": 57, "masteryi": 11, "missfortune": 21, "monkeyking": 62, "mordekaiser": 82, "morgana": 25, "nami": 267, "nasus": 75, "nautilus": 111, "nidalee": 76, "nocturne": 56, "nunu": 20, "olaf": 2, "orianna": 61, "ornn": 516, "pantheon": 80, "poppy": 78, "quinn": 133, "rakan": 497, "rammus": 33, "reksai": 421, "renekton": 58, "rengar": 107, "riven": 92, "rumble": 68, "ryze": 13, "sejuani": 113, "shaco": 35, "shen": 98, "shyvana": 102, "singed": 27, "sion": 14, "sivir": 15, "skarner": 72, "sona": 37, "soraka": 16, "swain": 50, "syndra": 134, "tahmkench": 223, "taliyah": 163, "talon": 91, "taric": 44, "teemo": 17, "thresh": 412, "tristana": 18, "trundle": 48, "tryndamere": 23, "twistedfate": 4, "twitch": 29, "udyr": 77, "urgot": 6, "varus": 110, "vayne": 67, "veigar": 45, "velkoz": 161, "vi": 254, "viktor": 112, "vladimir": 8, "volibear": 106, "warwick": 19, "xayah": 498, "xerath": 101, "xinzhao": 5, "yasuo": 157, "yorick": 83, "zac": 154, "zed": 238, "ziggs": 115, "zilean": 26, "zoe": 142, "zyra": 143}
+var champions_dic = {"aatrox": 266, "ahri": 103, "akali": 84, "alistar": 12, "amumu": 32, "anivia": 34, "annie": 1, "ashe": 22, "aurelionsol": 136, "azir": 268, "bard": 432, "blitzcrank": 53, "brand": 63, "braum": 201, "caitlyn": 51, "camille": 164, "cassiopeia": 69, "chogath": 31, "corki": 42, "darius": 122, "diana": 131, "draven": 119, "dr.mundo": 36, "ekko": 245, "elise": 60, "evelynn": 28, "ezreal": 81, "fiddlesticks": 9, "fiora": 114, "fizz": 105, "galio": 3, "gangplank": 41, "garen": 86, "gnar": 150, "gragas": 79, "graves": 104, "hecarim": 120, "heimerdinger": 74, "illaoi": 420, "irelia": 39, "ivern": 427, "janna": 40, "jarvaniv": 59, "jax": 24, "jayce": 126, "jhin": 202, "jinx": 222, "kalista": 429, "karma": 43, "karthus": 30, "kassadin": 38, "katarina": 55, "kayle": 10, "kayn": 141, "kennen": 85, "khazix": 121, "kindred": 203, "kled": 240, "kogmaw": 96, "leblanc": 7, "leesin": 64, "leona": 89, "lissandra": 127, "lucian": 236, "lulu": 117, "lux": 99, "malphite": 54, "malzahar": 90, "maokai": 57, "masteryi": 11, "missfortune": 21, "wukong": 62, "mordekaiser": 82, "morgana": 25, "nami": 267, "nasus": 75, "nautilus": 111, "nidalee": 76, "nocturne": 56, "nunu": 20, "olaf": 2, "orianna": 61, "ornn": 516, "pantheon": 80, "poppy": 78, "quinn": 133, "rakan": 497, "rammus": 33, "reksai": 421, "renekton": 58, "rengar": 107, "riven": 92, "rumble": 68, "ryze": 13, "sejuani": 113, "shaco": 35, "shen": 98, "shyvana": 102, "singed": 27, "sion": 14, "sivir": 15, "skarner": 72, "sona": 37, "soraka": 16, "swain": 50, "syndra": 134, "tahmkench": 223, "taliyah": 163, "talon": 91, "taric": 44, "teemo": 17, "thresh": 412, "tristana": 18, "trundle": 48, "tryndamere": 23, "twistedfate": 4, "twitch": 29, "udyr": 77, "urgot": 6, "varus": 110, "vayne": 67, "veigar": 45, "velkoz": 161, "vi": 254, "viktor": 112, "vladimir": 8, "volibear": 106, "warwick": 19, "xayah": 498, "xerath": 101, "xinzhao": 5, "yasuo": 157, "yorick": 83, "zac": 154, "zed": 238, "ziggs": 115, "zilean": 26, "zoe": 142, "zyra": 143}
 
 var champions_dicInv = {"1": "Annie", "2": "Olaf", "3": "Galio", "516": "Ornn", "5": "Xin Zhao", "6": "Urgot", "7": "LeBlanc", "8": "Vladimir", "9": "Fiddlesticks", "266": "Aatrox", "267": "Nami", "12": "Alistar", "13": "Ryze", "14": "Sion", "15": "Sivir", "16": "Soraka", "17": "Teemo", "18": "Tristana", "19": "Warwick", "20": "Nunu", "21": "Miss Fortune", "22": "Ashe", "23": "Tryndamere", "24": "Jax", "4": "Twisted Fate", "26": "Zilean", "27": "Singed", "28": "Evelynn", "29": "Twitch", "30": "Karthus", "31": "Cho'Gath", "32": "Amumu", "33": "Rammus", "34": "Anivia", "35": "Shaco", "36": "Dr. Mundo", "37": "Sona", "38": "Kassadin", "39": "Irelia", "40": "Janna", "41": "Gangplank", "42": "Corki", "43": "Karma", "44": "Taric", "45": "Veigar", "48": "Trundle", "10": "Kayle", "51": "Caitlyn", "53": "Blitzcrank", "54": "Malphite", "55": "Katarina", "56": "Nocturne", "57": "Maokai", "58": "Renekton", "59": "Jarvan IV", "60": "Elise", "61": "Orianna", "62": "Wukong", "63": "Brand", "64": "Lee Sin", "11": "Master Yi", "68": "Rumble", "69": "Cassiopeia", "72": "Skarner", "268": "Azir", "74": "Heimerdinger", "75": "Nasus", "76": "Nidalee", "77": "Udyr", "78": "Poppy", "79": "Gragas", "80": "Pantheon", "81": "Ezreal", "82": "Mordekaiser", "83": "Yorick", "84": "Akali", "85": "Kennen", "86": "Garen", "89": "Leona", "90": "Malzahar", "91": "Talon", "92": "Riven", "96": "Kog'Maw", "98": "Shen", "99": "Lux", "101": "Xerath", "102": "Shyvana", "103": "Ahri", "104": "Graves", "105": "Fizz", "106": "Volibear", "107": "Rengar", "110": "Varus", "111": "Nautilus", "112": "Viktor", "113": "Sejuani", "114": "Fiora", "115": "Ziggs", "117": "Lulu", "119": "Draven", "120": "Hecarim", "121": "Kha'Zix", "122": "Darius", "126": "Jayce", "127": "Lissandra", "131": "Diana", "133": "Quinn", "134": "Syndra", "136": "Aurelion Sol", "141": "Kayn", "142": "Zoe", "143": "Zyra", "67": "Vayne", "150": "Gnar", "25": "Morgana", "154": "Zac", "412": "Thresh", "157": "Yasuo", "161": "Vel'Koz", "163": "Taliyah", "420": "Illaoi", "421": "Rek'Sai", "427": "Ivern", "429": "Kalista", "432": "Bard", "201": "Braum", "202": "Jhin", "203": "Kindred", "164": "Camille", "222": "Jinx", "223": "Tahm Kench", "50": "Swain", "236": "Lucian", "238": "Zed", "240": "Kled", "497": "Rakan", "498": "Xayah", "245": "Ekko", "254": "Vi"}
 
@@ -49,14 +49,15 @@ function search() {
     var champions = $$(".champion_input");
     var query = "";
     for (var node = 0; node < champions.length; ++node) {
-        var champId = champions_dic[champions[node].value.toLowerCase().replace(" ","")]
+        var champ = champions[node].value.toLowerCase().replace(" ","");
+        var champId = champions_dic[champ];
         if (champId) {
             query += "&champ" + (node + 1) + "=" + champId;
         }
     }
     if (query.length > 0) {
         query = query.substring(1);
-        callAjax("http://127.0.0.1:8000/bestcomp?" + query, searchResponse);
+        callAjax("http://127.0.0.1:8000/nextbestchamp?" + query, searchResponse);
     }
 }
 
@@ -73,9 +74,15 @@ function searchResponse(response) {
     response = JSON.parse(response);
     var winPercent = response['win_percent'];
     var total_games = response['total_games'];
+    var wins = response["wins"];
     var given_champs = response["given_champs"];
+    var best_champ = response["champId"];
+
     $("#winPercentage").innerHTML = (winPercent * 100).toFixed(2);
     $("#totalGames").innerHTML = total_games;
+    $("#bestChamp").innerHTML = champions_dicInv[best_champ];
+    $("#wins").innerHTML = wins;
+    /*
     Object.keys(response).forEach(function(key) {
         champions = key.split(",");
         if (champions.length === 5) {
@@ -93,6 +100,7 @@ function searchResponse(response) {
             drawNewResult(champIdsToName(champions), response[key]['wins'] / response[key]['total_games'], response[key]['total_games']);
         }
     });
+    */
 }
 
 function drawNewResult(champ_list, winningPercentage, totalGames) {
